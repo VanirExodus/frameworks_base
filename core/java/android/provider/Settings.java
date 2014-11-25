faef663caa13cab26dc0f8c9c9092b426cafed2a
cmorlok <ChristianMorlok@gmail.com>
2014-11-25 13:31:56 +0000

    [1/2] Base: Hide power menu on lockscreen
    Adds an option to enable/disable power menu on lockscreen.
    
    Change-Id: Ia596cfab74e065240b5c2636000ea53b74be01d6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3470,6 +3470,12 @@ public final class Settings {
         public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
 
         /**
+         * This preference enables showing the power menu on LockScreen.
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
