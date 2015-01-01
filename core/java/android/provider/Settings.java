5e06dacf36849e94cc35bf675fbe07b47e7d5a0b
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-01 11:41:00 +0000

    Quick Settings: Auto collapse panel (1/2)
    Since we have some tiles which display detailed view like CellularTile I've
    added the call to the tiles which will use it instead of globally.
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
    	packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
    
    Change-Id: Ide1a1aad9e0a34bd496b4d24be66bf028b30bac7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3420,6 +3420,12 @@ public final class Settings {
          */
         public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
 
+        /** Whether to collapse panel when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_COLLAPSE_PANEL = "quick_settings_collapse_panel";
+
         /**
          * An intent (a flattened Uri String) to launch when user voice launch
          * action is detected. An empty or null string will launch the default
