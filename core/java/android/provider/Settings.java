19ce46abaa4c806f39fcb19aa44d4e2489006778
rascarlo <rascarlo@gmail.com>
2016-04-06 04:11:02 +0530

    Volume Rocker wake (1/2)
    Commit by rascarlo, adapted for m by KreAch3R
    
    Change-Id: I38a10b5d0fee3407ca348403aa0b52952175d9f1
    
    Marshmallow: If a wake key is disabled by the user, do not wake from
    doze.
    
    Adapted for M by @KreAch3R
    
    Currently, any wake key will wake the device from a doze, even if that
    key has not been enabled as a wake key in Settings.
    
    If the device is 'dreaming' in the Doze state, check if the user has
    explicitly disabled the wake key (or never enabled the setting in the
    first place) before waking the device.
    
    Conflicts:
    
    policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    volume rocker wake: add volume keys to isWakeKey KeyEvents
    
    Adapted for marshmallow
    
    follow CyanogenMod/android_frameworks_base@fedeb99
    
    Change-Id: I63de2e4e471d10d3519ab936d37a3258f908393c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3233,6 +3233,10 @@ public final class Settings {
          */
         public static final String WIFI_AUTO_CONNECT_TYPE = "wifi_auto_connect_type";
 
+       /* Volume rocker wake
+        * @hide
+        */
+        public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
